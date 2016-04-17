a4d0a37d4c9e75fc245a5d63a7dbba85e27b282b
Philbert Yoon <philbert@felixlab.io>
2016-04-17 22:37:39 +0900

    まだチャプター4のTemplates-_-;;

--- go-prog-lang/ch4/github_issues/main.go
+++ go-prog-lang/ch4/github_issues/main.go
@@ -1,4 +1,4 @@
-package main
+package github_issues
 
 import (
 	"encoding/json"
@@ -19,13 +19,13 @@ type IssuesSearchResult struct {
 }
 
 type Issue struct {
-	Number   int
-	HTMLURL  string `json:"html_url"`
-	Title    string
-	State    string
-	User     *User
-	CreateAt time.Time `json:"created_at"`
-	Body     string
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"created_at"`
+	Body      string
 }
 
 type User struct {
---
+++ go-prog-lang/ch4/issuesreport/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/patriz/on-the-go/go-prog-lang/ch4/github_issues"
+)
+
+const templ = `{{.TotalCount}} issues:
+{{range .Items}}----------------------------------------
+Number: {{.Number}}
+User:   {{.User.Login}}
+Title:  {{.Title | printf "%.64s"}}
+Age:    {{.CreatedAt | daysAgo}} days
+{{end}}`
+
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
+}
+
+var report = template.Must(
+	template.New("issuelist").
+		Funcs(template.FuncMap{"daysAgo": daysAgo}).
+		Parse(templ))
+
+func noMust() {
+	report, err := template.New("report").
+		Funcs(template.FuncMap{"daysAgo": daysAgo}).
+		Parse(templ)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := github_issues.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	result, err := github_issues.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
