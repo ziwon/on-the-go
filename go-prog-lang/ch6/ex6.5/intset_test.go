c4d5d5d628bcdea56ace8f53173997a4c1c90251
Philbert Yoon <philbert@felixlab.io>
2016-07-21 01:10:50 +0900

    Solve ex6.5 (You know, dead simple!)

---
+++ go-prog-lang/ch6/ex6.5/intset_test.go
@@ -0,0 +1,21 @@
+package intset
+
+import "testing"
+
+func TestElems(t *testing.T) {
+	y := []int{1, 2, 3, 144, 4095, 8129, 1048575}
+
+	var x IntSet
+	for _, e := range y {
+		x.Add(e)
+	}
+
+	for i, e := range x.Elems() {
+		if e != y[i] {
+			t.Error("Oops...")
+		}
+	}
+
+	// Testing in 32Bit Platform
+	// https://play.golang.org/p/TNGhTw703I
+}
