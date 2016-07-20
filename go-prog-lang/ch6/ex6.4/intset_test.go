caf4179c2d4b2699e1702a5a54a9a98e97808e41
Philbert Yoon <philbert@felixlab.io>
2016-07-20 23:56:14 +0900

    Update test code in ex6.4 as a dummy commit

--- go-prog-lang/ch6/ex6.4/intset_test.go
+++ go-prog-lang/ch6/ex6.4/intset_test.go
@@ -1,17 +1,18 @@
 package intset
 
 import "testing"
-import "fmt"
 
 func TestElems(t *testing.T) {
+	y := []int{1, 2, 3, 144, 4095, 8129, 1048575}
+
 	var x IntSet
+	for _, e := range y {
+		x.Add(e)
+	}
 
-	x.Add(1)
-	x.Add(2)
-	x.Add(3)
-	x.Add(244)
-
-	for _, e := range x.Elems() {
-		fmt.Println(e)
+	for i, e := range x.Elems() {
+		if e != y[i] {
+			t.Error("Oops...")
+		}
 	}
 }
